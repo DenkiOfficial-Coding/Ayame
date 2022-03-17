20c5a7c19f0ff0e12e03a89ff26293d29ce77a7e
DenkiOfficial-Coding <87896075+DenkiOfficial-Coding@users.noreply.github.com>
2022-03-17 16:08:28 +0700

    Update shard.js

--- shard.js
+++ shard.js
@@ -2,7 +2,7 @@ const config = require('./config.json')
 
 const { ShardingManager } = require('discord.js');
 const shard = new ShardingManager('./index.js', {
-  token: "OTQ5MTU0NTE1MDgzNDc3MDIz.YiGPBg.HT4EVgu0Hvex6UEjHem2b8wtWNk",
+  token: "",
   autoSpawn: true
 });
 
